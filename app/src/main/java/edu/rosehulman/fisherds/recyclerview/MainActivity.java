02110b590a6990b6473033887fddec5294d33d4d
David Fisher <fisherds@rose-hulman.edu>
2018-05-31 16:37:58 +0800

    Added the names of everyone in the class

--- app/src/main/java/edu/rosehulman/fisherds/recyclerview/MainActivity.java
+++ app/src/main/java/edu/rosehulman/fisherds/recyclerview/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -18,12 +21,27 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
+    RecyclerView recyclerView = findViewById(R.id.recycler_view);
+    // Configure the recycler view
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//    recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+    recyclerView.setHasFixedSize(true);
+
+    // Connect the Recycler View to our Adapter
+    final NameAdapter nameAdapter = new NameAdapter(this);
+
+    // TODO: When ready uncomment the line below
+//    recyclerView.setAdapter(nameAdapter);
+    nameAdapter.addName();
+    nameAdapter.addName();
+    nameAdapter.addName();
+    nameAdapter.addName();
+
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
+        nameAdapter.addName();
       }
     });
   }
